615e85ad789c14b4eb15dfa86db9f60d54e4bdf0
Sachin <ysachin0709@gmail.com>
2024-05-03 17:30:03 +0530

    updated

--- src/main/java/com/turf/controller/HomeController.java
+++ src/main/java/com/turf/controller/HomeController.java
@@ -4,48 +4,58 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class HomeController {
-	
+
 	@GetMapping("/")
 	public String index(Model model) {
-		
+
 		model.addAttribute("title", "Home Page");
 		return "index";
 	}
-	
+
 	@GetMapping("/grounds")
 	public String grounds(Model model) {
-		
-		model.addAttribute("title","View All Ground");
+
+		model.addAttribute("title", "View All Ground");
 		return "grounds";
 	}
+
 	@GetMapping("/about")
-	public String aboutUs() {
-		
+	public String aboutUs(Model model) {
+
+		model.addAttribute("title", "About Us");
 		return "about";
 	}
+
 	@GetMapping("/contact")
-	public String contactUs() {
-		
+	public String contactUs(Model model) {
+
+		model.addAttribute("title", "Any Query Conatct Us");
 		return "contact";
 	}
+
 	@GetMapping("/register")
-	public String register() {
-		
+	public String register(Model model) {
+		model.addAttribute("title", "Register Here");
 		return "register";
 	}
-	
+
 	@GetMapping("/login")
-	public String login() {
-		
+	public String login(Model model) {
+		model.addAttribute("title", "Login Here");
 		return "login";
 	}
-	@GetMapping("/viewground")
-	public String viewground() {
-		
+
+	@GetMapping("/ground")
+	public String viewground(Model model) {
+		model.addAttribute("title", "Single Ground");
 		return "viewground";
+	}
+	@GetMapping("/payment")
+	public String payment(Model model) {
+		model.addAttribute("title", "Make Payment");
+		return "payment";
 	}
 
 }
